f2e3bfe18c829f31042613de225f7c6243cdd303
shcherbinina-yelyzaveta <scherbininaliza@gmail.com>
2018-01-28 20:29:02 +0200

    change CartDAO:findAll

--- src/com/company/store/database/CartDAO.java
+++ src/com/company/store/database/CartDAO.java
@@ -1,13 +1,34 @@
 package com.company.store.database;
 
 import com.company.store.model.Cart;
+import com.company.store.model.Product;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CartDAO extends AbstractDAO<Integer, Cart> {
+    private static final String SQL_SELECT_ALL_CARTS = "SELECT * FROM cart";
     @Override
     public List<Cart> findAll() {
-        return null;
+        List<Cart> carts = new ArrayList<>();
+        try (Connection connection = ConnectorDB.getConnection();
+             Statement statement = connection.createStatement()) {
+            ResultSet rs = statement.executeQuery(SQL_SELECT_ALL_CARTS);
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                Double price = rs.getDouble("price");
+                //todo: add list<Product>
+            }
+        } catch (SQLException e) {
+            System.err.println("SQL Exception (request or table failed):" + e);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return carts;
     }
 
     @Override
